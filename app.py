2897741aad781e2352e5e9d8d1db6f67b063ce32
Karan Kakati <karanpkofficial@gmail.com>
2023-06-06 22:53:14 +0530

    began with API integration

--- app.py
+++ app.py
@@ -9,8 +9,13 @@ import argparse
 
 from fileformats import tuple_fileformats as ext
 
+# import appwrite libraries to use their storage bucket services
+from appwrite.client import Client
+from appwrite.services.storage import Storage
+
+
 # the messages below will be used relatively to prompt that there is an error with files
-INVALID_FILETYPE_MSG = "ERROR: %s is either invalid or we do not support this file. Please re-check %s."
+INVALID_FILETYPE_MSG = "ERROR: %s is either invalid or we do not support this file. Please re-check the file."
 INVALID_PATH_MSG = "ERROR: Looks like file path/name is invalid. Path '%s' does not exist."
 
 files = ['ada.txt','karan.txt','file.txt']
@@ -37,6 +42,16 @@ def validate_filepath(filename):
     """Validates file path in the system and returns in 'bool'"""
     return os.path.exists(filename)
 
+def login():
+    username = input("What's your username? ")
+    password = input("What's your password? ")
+
+    print("Hi, %s. You are in!"% username)
+
+def logout():
+    print("You are logged out.")
+    
+
 def upload(args):
     """
     Uploads the specified file to the cloud.
@@ -49,23 +64,40 @@ def upload(args):
 
     # validate file name/path
     validate_file(filename)
+
+    path = Path(__file__)
+    print(path)
+    # help(storage.create_file())
+
+    client = Client()
+    storage = Storage(client)
+
+    client.set_endpoint('https://cloud.appwrite.io/v1')
+    client.set_project('647c49a7e79df168b264')
+    client.set_key('7e62fbf81b373436fc3b6a7b798ba14a8fc6b2e7dcf1ea7b865b96ef10cc2ef2d540e883bff4515fb68f09b7fab128fd2278c63b0f99a42a60ea48330819302f85bf96494a7033f2915b8198993384cf25270460c8aa27d70dbf84874cc30b5408bd7e07c52c7e9d6ecfc499cfd7de6ed6016abbe0b5386bd19aef5716409f93')
+
+    result = storage.create_file('647f207c336d08d20e1f',ID.unique(),filename)
+
+    
+
+    print(result)
     
     # check if file already exists, if False, upload the specified file and notify user
-    if filename in files:
-        # if True, ask user to whether replace the file else take no action
-        response = input("%s already exists. Would you like to update the file [y/n]?"%filename)
-        if 'y' in response:
-            files.remove(filename)
-            files.append(filename)
-            print("%s updated successfully 💯." %filename)
-            return
-        else:
-            print("Rest free 🙌🏼. No action taken.")
-            return
-    else:
-        files.append(filename)
-        print("Yep there it goes 🚀. %s uploaded successfully." %filename)
-        return
+    # if filename in files:
+    #     # if True, ask user to whether replace the file else take no action
+    #     response = input("%s already exists. Would you like to update the file [y/n]?"%filename)
+    #     if 'y' in response:
+    #         files.remove(filename)
+    #         files.append(filename)
+    #         print("%s updated successfully 💯." %filename)
+    #         return
+    #     else:
+    #         print("Rest free 🙌🏼. No action taken.")
+    #         return
+    # else:
+    #     files.append(filename)
+    #     print("Yep there it goes 🚀. %s uploaded successfully." %filename)
+    #     return
 
 def delete(args):
     """
@@ -155,7 +187,7 @@ def main():
     parser.add_argument("-lin", "--login", type=str, nargs='*',
                         metavar="login", help="Login to your personal cloud")
     
-    parser.add_argument("-lout", "--lougout", type=str, nargs='*',
+    parser.add_argument("-lout", "--logout", type=str, nargs='*',
                         metavar="logout", help="Logout from your personal cloud")
 
     parser.add_argument("-up", "--upload", type=str, nargs='*',
@@ -182,7 +214,11 @@ def main():
     args = parser.parse_args()
 
     # call the functions depending on the type of arg
-    if args.upload != None:
+    if args.login != None:
+        login()
+    elif args.logout != None:
+        logout()
+    elif args.upload != None:
         upload(args)
     elif args.delete != None:
         delete(args)
